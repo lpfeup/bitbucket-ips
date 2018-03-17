36ba3200798e9c8b9bb99d9b90e8ba04ddaf3e5f
lpfeup <lpfeup@users.noreply.github.com>
2018-03-17 00:42:12 +0000

    update to working version. add cheerio-tableparser dependency. updated README.md update tests.

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 var request = require ('superagent');
 var cheerio = require ('cheerio');
-var URL = 'https://confluence.atlassian.com/display/BITBUCKET/What+are+the+Bitbucket+IP+addresses+I+should+use+to+configure+my+corporate+firewall';
+var cheerioTableParser = require ('cheerio-tableparser');
+var URL = 'https://confluence.atlassian.com/bitbucket/what-are-the-bitbucket-cloud-ip-addresses-i-should-use-to-configure-my-corporate-firewall-343343385.html';
 
 function parse (html) {
 	var $ = cheerio.load(html);
-	var obj = {};
-	$('.confluenceTable tbody tr').each(function(){
-		var left = true;
-		$(this).find('td').each(function(){
-			var text = $(this).text().trim();
-			if (left){
-				obj.inbounds = obj.inbounds || []; 
-				if (text) obj.inbounds.push(text);
-				left = false;
-			} else {
-				obj.outbounds = obj.outbounds || [];
-				if (text) obj.outbounds.push(text);
-			}
-		});
-	});
+	cheerioTableParser(cheerio);
+
+	var obj = {
+		ipv4: {
+			inbounds: [],
+			outbounds: []
+		},
+		ipv6: {
+			inbounds: [],
+			outbounds: []
+		}
+	};
+
+	var parsedTable = parseTable($('table.confluenceTable').eq(0), 2);
+	obj.ipv4.inbounds.push(...parsedTable[0]);
+	obj.ipv4.outbounds.push(...parsedTable[1]);
+	obj.ipv6.inbounds.push(...parsedTable[2]);
+	obj.ipv6.outbounds.push(...parsedTable[3]);
+
+	parsedTable = parseTable($('table.confluenceTable').eq(1), 1);
+	obj.ipv4.inbounds.push(...parsedTable[0]);
+	obj.ipv6.inbounds.push(...parsedTable[1]);
+
+	parsedTable = parseTable($('table.confluenceTable').eq(2), 1);
+	obj.ipv4.outbounds.push(...parsedTable[0]);
+
+	parsedTable = parseTable($('table.confluenceTable').eq(3), 1);
+	obj.ipv4.outbounds.push(...parsedTable[0]);
+
 	return obj;
+}
+
+function parseTable (table, headers) {
+	var parsedTable = table.parsetable(false, false, true);
+	parsedTable = parsedTable.map(col => col.filter((elem, i) => i >= headers && elem.length > 0));
+
+	return parsedTable;
 }
 
 module.exports = function(cb){
--- test.js
+++ test.js
@@ -4,8 +4,10 @@ describe ('Bitbucket whitelist', function(){
   it('should get the ips', function(done){
     ips (function(err, obj) {
       if (err) return done(err);
-      obj.outbounds.length.should.above(0); 
-      obj.outbounds.length.should.above(0); 
+      obj.ipv4.inbounds.length.should.above(0); 
+      obj.ipv4.outbounds.length.should.above(0); 
+      obj.ipv6.inbounds.length.should.above(0); 
+      obj.ipv6.outbounds.length.should.above(0); 
       done(); 
     });
   });
